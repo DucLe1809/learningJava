db76cbdcbde93cb21cd1b8973ac794f1d753999a
xxdule <duc.h.le@tuni.fi>
2025-06-16 10:16:01 +0700

    Singleton Implementation

---
+++ Singleton/src/main/java/org/example/DatabaseConfigSingleton.java
@@ -0,0 +1,94 @@
+package org.example;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class DatabaseConfigSingleton {
+    private String host;
+    private int port;
+    private String user;
+    private String password;
+
+    private static volatile DatabaseConfigSingleton obj;
+
+    private DatabaseConfigSingleton() {}
+
+    public static DatabaseConfigSingleton getInstance()
+    {
+        if (obj == null)
+        {
+            synchronized (DatabaseConfigSingleton.class)
+            {
+                if (obj == null)
+                {
+                    obj = new DatabaseConfigSingleton();
+                }
+            }
+        }
+        return obj;
+    }
+
+    public void loadFromFile(String path) {
+        try
+        {
+            File myFile = new File(path);
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine().trim();
+
+                if (line.isEmpty() || line.startsWith("#")) {
+                    continue;
+                }
+
+                String[] parts = line.split("=", 2);
+                if (parts.length < 2) {
+                    continue;
+                }
+                String key = parts[0].trim();
+                String value = parts[1].trim();
+
+                switch (key) {
+                    case "host":
+                        this.host = value;
+                        break;
+                    case "port":
+                        this.port = Integer.parseInt(value);
+                        break;
+                    case "user":
+                        this.user = value;
+                        break;
+                    case "password":
+                        this.password = value;
+                        break;
+                    default:
+                        System.out.println("Invalid key: " + key);
+                        break;
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number format!");
+        }
+    }
+
+    // Getter function
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
---
+++ Singleton/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConfigSingleton configSingleton = DatabaseConfigSingleton.getInstance();
+        configSingleton.loadFromFile("file.txt");
+
+        System.out.println("Host: " + configSingleton.getHost());
+        System.out.println("Port: " + configSingleton.getPort());
+    }
+}
