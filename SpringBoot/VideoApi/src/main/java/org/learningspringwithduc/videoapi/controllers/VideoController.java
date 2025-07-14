ba4421e388d6455287b2170816e2f38058216798
xxdule <duc.h.le@tuni.fi>
2025-07-14 11:06:06 +0700

    minor change in attributes in controller

--- SpringBoot/VideoApi/src/main/java/org/learningspringwithduc/videoapi/controllers/VideoController.java
+++ SpringBoot/VideoApi/src/main/java/org/learningspringwithduc/videoapi/controllers/VideoController.java
@@ -1,11 +1,9 @@
 package org.learningspringwithduc.videoapi.controllers;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.learningspringwithduc.videoapi.entities.VideoEntities;
-import org.learningspringwithduc.videoapi.repositories.VideoRepositories;
+import org.learningspringwithduc.videoapi.repositories.VideoService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,10 +11,10 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 public class VideoController {
-    private final VideoRepositories  videoRepositories;
+    private final VideoService videoService;
 
     @GetMapping("/videos")
     public List<VideoEntities> getAllVideos(){
-        return videoRepositories.findAll();
+        return videoService.getAllVideos();
     }
 }
